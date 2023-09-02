c0d84897bc973542ab4b0dfdffae9cb10b03785e
AminDqn <59019810+amindqn@users.noreply.github.com>
2023-09-02 21:50:51 +0330

    dqn

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,12 +9,10 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Amin Dehqan </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
-            <br /> I am a final year student pursuing an Integrated MSc (IMSc)
-            in Maths and Computing at BIT Mesra.
+            from <span className="purple"> Tehran, Iran.</span>
+            <br /> I am a graduate of Engineering Physics from University of Tehran.
             <br />
-            Additionally, I am currently employed as a software developer at
-            Juspay.
+            Additionally, I am currently employed as a software developer.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
@@ -26,15 +24,12 @@ function AboutCard() {
             <li className="about-activity">
               <ImPointRight /> Writing Tech Blogs
             </li>
-            <li className="about-activity">
-              <ImPointRight /> Travelling
-            </li>
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">AminDqn</footer>
         </blockquote>
       </Card.Body>
     </Card>
